c3cdc463546115552d38508a9388fab3701869d2
MChandraR <chandramadhan04@gmail.com>
2026-03-02 22:46:56 +0700

    Add add DatasetManager class to handle training data pre-processing step

--- app/src/main/java/com/wasterec/app/utils/ImageUtil.kt
+++ app/src/main/java/com/wasterec/app/utils/ImageUtil.kt
@@ -9,6 +9,7 @@ import android.provider.MediaStore
 import androidx.annotation.RequiresApi
 import android.graphics.Matrix
 import kotlin.math.min
+import kotlin.random.Random
 
 fun resizeAndCropCenter(bitmap: Bitmap, targetSize: Int = 224): Bitmap {
     val width = bitmap.width
@@ -61,4 +62,63 @@ fun forceSoftwareBitmap(bitmap: Bitmap): Bitmap {
     val softwareBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, false)
     bitmap.recycle()
     return softwareBitmap
+}
+
+/**
+ * Memutar bitmap berdasarkan derajat tertentu (misal: 90f, 180f, 270f).
+ */
+fun rotateBitmap(bitmap: Bitmap, degrees: Float): Bitmap {
+    val matrix = Matrix()
+    matrix.postRotate(degrees)
+    return Bitmap.createBitmap(
+        bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true
+    )
+}
+
+fun flipHorizontal(bitmap: Bitmap): Bitmap {
+    val matrix = Matrix()
+    // Scale -1 pada sumbu X akan membalikkan gambar secara horizontal
+    matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
+    return Bitmap.createBitmap(
+        bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true
+    )
+}
+
+fun flipVertical(bitmap: Bitmap): Bitmap {
+    val matrix = Matrix()
+    // Scale 1f pada X (tetap), -1f pada Y (terbalik secara vertikal)
+    matrix.postScale(1f, -1f, bitmap.width / 2f, bitmap.height / 2f)
+
+    return Bitmap.createBitmap(
+        bitmap,
+        0,
+        0,
+        bitmap.width,
+        bitmap.height,
+        matrix,
+        true
+    )
+}
+
+fun applyRandomAugmentation(bitmap: Bitmap): Bitmap {
+    var result = bitmap
+
+    // Acak Flip (50% peluang)
+    if ((0..1).random() == 1) {
+        result = flipHorizontal(result)
+    }
+
+    // Acak Rotasi (Pilih salah satu sudut)
+    val angles = listOf(0f, 90f, 180f, 270f)
+    val randomAngle = angles.random()
+    if (randomAngle != 0f) {
+        result = rotateBitmap(result, randomAngle)
+    }
+
+    return result
+}
+
+fun generateBooleanWithChance(chanceOfTrue: Double): Boolean {
+    require(chanceOfTrue in 0.0..1.0) { "Chance must be between 0.0 and 1.0" }
+    return Random.nextDouble() < chanceOfTrue
 }
