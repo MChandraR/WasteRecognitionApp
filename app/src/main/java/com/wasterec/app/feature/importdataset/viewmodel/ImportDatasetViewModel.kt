99426b3913d3a9de83875649d67d625ca9932fee
MChandraR <chandramadhan04@gmail.com>
2026-04-05 00:26:47 +0700

    Fix label count validation : must meet requirement max and min before pre-processing

--- app/src/main/java/com/wasterec/app/feature/importdataset/viewmodel/ImportDatasetViewModel.kt
+++ app/src/main/java/com/wasterec/app/feature/importdataset/viewmodel/ImportDatasetViewModel.kt
@@ -2,8 +2,10 @@ package com.wasterec.app.feature.importdataset.viewmodel
 
 import android.app.Application
 import android.content.Context
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.AndroidViewModel
 import androidx.navigation.NavHostController
@@ -23,6 +25,7 @@ class ImportDatasetViewModel(
 
     val datasetManager = DatasetManager(trainignDataset)
     var classCount : MutableList<Int> = mutableStateListOf(0,0,0,0,0,0)
+    val isClassCountMeetRequirement : MutableState<Boolean> = mutableStateOf(false)
 
     fun getTrainingDatasetCount():Int{
         return trainignDataset.size
@@ -32,6 +35,23 @@ class ImportDatasetViewModel(
         datasetManager.getEachLabelCount().forEachIndexed { idx, value ->
             classCount[idx] = value
         }
+    }
+
+    fun validateClassCount(){
+        var isMeetMinMaxCount = true
+        datasetManager.getEachLabelCount().forEachIndexed { idx, value ->
+            println("Label ${datasetClassList[idx].className} : $value , minium : ${datasetClassList[idx].minimunCount}")
+            if(value < datasetClassList[idx].minimunCount ){
+                isMeetMinMaxCount = false
+            }else if(value > datasetClassList[idx].maximumCount){
+                isMeetMinMaxCount = false
+            }
+        }
+        isClassCountMeetRequirement.value = isMeetMinMaxCount
+    }
+
+    fun resetState(){
+        isClassCountMeetRequirement.value = false
     }
 
 }
